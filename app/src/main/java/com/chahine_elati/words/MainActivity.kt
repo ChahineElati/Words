c85453e4d33a44a98a731574705c14da6fc9d800
ChahineElati <chahinosaiyan@gmail.com>
2021-11-15 09:57:37 +0100

    Initial commit

---
+++ app/src/main/java/com/chahine_elati/words/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.chahine_elati.words
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.chahine_elati.words.adapter.LetterAdapter
+import com.chahine_elati.words.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+
+}
